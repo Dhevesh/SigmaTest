21a90dd2081463b99503567ee299eb42c71a395a
IMS\Dheveshp <dheveshp@gmail.com>
2019-10-18 09:22:44 +0100

    initial routes setup

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,5 +1,16 @@
 document.onreadystatechange = function(){
     if (document.readyState === 'complete'){
         $('#loading').remove();
-    } 
+
+
+        $.ajax({
+            url : 'http://localhost:3000/project/'
+        }).done(getProjects);
+    
+        function getProjects(projects){
+            console.log(projects.title);
+        }
+    }
+    
+    
 }
--- server/app.js
+++ server/app.js
@@ -3,20 +3,30 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded( { extended : true } ));
+const projectRoutes = require('./controllers/project');
+const testRunRoutes = require('./controllers/testruns');
+const testSuitesRoutes = require('./controllers/testsuites');
+const testCaseRoutes = require('./controllers/testcase');
 
-mongoose.connect('mongodb://localhost:27017/sigmatest_db',  { useNewUrlParser : true, useUnifiedTopology : true } );
+app.use(bodyParser.urlencoded({ extended: true }));
 
-let simpleJSONResponse = { myDummyData : 'This is a dummy data response' };
+mongoose.connect('mongodb://localhost:27017/sigmatest_db', { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.use(function(req, res, next) {
+let simpleJSONResponse = { myDummyData: 'This is a dummy data response' };
+
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:8080");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     next();
-  });
+});
 
-app.get('/', (req, res)=>{
+app.use('/project', projectRoutes);
+app.use('/project/:id/testruns', testRunRoutes);
+app.use('/project/:id/testsuites', testSuitesRoutes);
+app.use('/project/:id/testsuite/:testsuite_id/testcases', testCaseRoutes);
+
+app.get('/', (req, res) => {
     res.json(simpleJSONResponse);
 });
 
@@ -27,6 +37,6 @@ app.get('/', (req, res)=>{
 
 
 let PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log('serving app on port ' + PORT);
 });
